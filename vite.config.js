84a2a79be5b0d847743efce11337746b94b43180
XiGua-frt <369546137@qq.com>
2026-04-17 18:29:53 +0800

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
-    plugins: [react()]
+    plugins: [react()],
+    optimizeDeps: {
+        include: [
+            'react-syntax-highlighter',
+            'react-syntax-highlighter/dist/esm/languages/prism/markdown',
+            'react-syntax-highlighter/dist/esm/languages/prism/typescript',
+            'react-syntax-highlighter/dist/esm/languages/prism/javascript',
+            'react-syntax-highlighter/dist/esm/languages/prism/tsx',
+            'react-syntax-highlighter/dist/esm/styles/prism',
+            'refractor',
+            'refractor/core',
+            'refractor/lang/markdown.js',
+            'refractor/lang/typescript.js',
+            'refractor/lang/javascript.js'
+        ]
+    },
+    build: {
+        commonjsOptions: {
+            transformMixedEsModules: true
+        }
+    }
 });
